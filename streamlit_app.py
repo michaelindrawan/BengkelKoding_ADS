692b289f0b98b5d05817d8c41b1a7533b0d383c1
michaelindrawan <96099551+Dokaebi20@users.noreply.github.com>
2024-01-05 01:35:17 +0700

    Updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -97,7 +97,7 @@ y = df_clean['target']
 smote = SMOTE(random_state=42)
 X, y = smote.fit_resample(X, y)
 #Melakukan standardisasi pada dataset
-X = scaler.fit_transform(X)
+# X = scaler.fit_transform(X)
 # Membaca model yang telah di-train sebelumnya
 model = pickle.load(open("models/xgb_model.pkl", 'rb'))
 # Melakukan prediksi menggunakan model pada dataset yang telah di-preprocess
