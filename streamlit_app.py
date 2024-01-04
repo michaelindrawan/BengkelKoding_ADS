5eca537eb273a10a79e6b62e6cd06cc964882588
michaelindrawan <96099551+Dokaebi20@users.noreply.github.com>
2024-01-05 01:23:37 +0700

    Updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,7 +92,7 @@ y = df_clean['target']
 smote = SMOTE(random_state=42)
 X, y = smote.fit_resample(X, y)
 # Membaca model yang telah di-train sebelumnya
-model = pickle.load(open("models/hungarian_model.pkl", 'rb'))
+model = pickle.load(open("models/xgb_model.pkl", 'rb'))
 # Melakukan prediksi menggunakan model pada dataset yang telah di-preprocess
 y_pred = model.predict(X)
 accuracy = accuracy_score(y, y_pred)
